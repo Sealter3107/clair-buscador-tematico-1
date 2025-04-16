1eb6e544457f634f81016704a98389e0b8c62577
Sealter3107 <sebastianpantoja31@gmail.com>
2025-04-16 16:45:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-import pandas as pd
-import os
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Cargar DataFrame global
-df = pd.read_excel("data.xlsx")
-df = df.fillna("")
-
-# HTML principal
-@app.get("/", response_class=HTMLResponse)
-async def get_home():
+@app.get("/")
+def serve_root():
     return FileResponse("static/index.html")
-
-# Endpoint para los datos en formato JSON
-@app.get("/data")
-async def get_data():
-    return df.to_dict(orient="records")
